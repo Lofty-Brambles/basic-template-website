67b61a6a8a87e5cc9af657915be522924eb7cfa0
Lofty | Brambles <prantikdas2004@gmail.com>
2022-09-16 17:57:41 +0530

    ✨ feat | Adds pages and the server script

---
+++ script.js
@@ -0,0 +1,36 @@
+const https = require("https");
+const fs = require("fs/promises");
+
+try {
+  const everyFile = await fs.readdir("./", {
+    encoding: "utf-8",
+    withFileTypes: true,
+  });
+
+  const files = everyFile
+    .filter((e) => /\.html$/gi.test(e.name))
+    .map((e) =>
+      e.name === "index.html" ? "" : e.name.slice(0, e.name.length - 5)
+    );
+
+  https
+    .createServer(async (req, res) => {
+      res.statusCode = 200;
+      const url = req.url.slice(1);
+      res.setHeader("Content-Type", "text/html");
+
+      if (files.includes(url)) {
+        const name = url === "" ? "index" : url;
+        const file = await fs.readFile(`./${name}.html`, "utf-8");
+        res.end(file);
+      } else {
+        const file = await fs.readFile(`./404.html`, "utf-8");
+        res.end(file);
+      }
+    })
+    .listen(3000, () => {
+      console.log("Connected.");
+    });
+} catch (e) {
+  console.log("The details of the pages weren't found!");
+}
